6a1e76735210bb48fbfded8ecb7a5f8297a11f5a
Niklas Bieda <Niklas.Bieda@student.uts.edu.au>
2023-08-30 14:06:03 +1000

    resolved namespace error

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
     private string nameOfGameObject;
     private int i;
